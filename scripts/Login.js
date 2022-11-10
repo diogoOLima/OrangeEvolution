fc1da456366a4c28766013a9fa35bb8b11e39842
Lucas_Passos <lucas_passos@msn.com>
2022-11-10 16:34:54 -0300

    inicio da logica de validacao de login

--- scripts/Login.js
+++ scripts/Login.js
@@ -1,12 +1,14 @@
 const url = "https://orangevolution.herokuapp.com/usuarios"
+const botao = document.getElementById('send');
+const usuarioErro = document.getElementById('erro-login');
+const senhaErro = document.getElementById('erro-senha');
 
 function getUsers(){
     axios.get(url)
         .then(response => {
             const data = response.data
-            // renderResults.textContent = JSON.stringify(data)
-            const login = document.querySelector('#ilogin').value
-            const password = document.querySelector('#ipassword').value
+            const login = document.querySelector('#ilogin').value;
+            const password = document.querySelector('#ipassword').value;
             
             let userValid = {
                 id:'',
@@ -15,10 +17,9 @@ function getUsers(){
                 isAdmin: ''
             }
 
-            var userFound = false
             data.forEach(element => {
                 if(login == element.usuario && password == element.senha){
-                    userFound = true
+
                     let token = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2);
                     localStorage.setItem('token', token)
                     userValid = {
@@ -27,17 +28,45 @@ function getUsers(){
                         password: element.senha,
                         isAdmin: element.isAdmin
                     }
-                    localStorage.setItem('userLogado', JSON.stringify(userValid))
+                    localStorage.setItem('userLogado', JSON.stringify(userValid));
                     if(element.isAdmin == 1){
-                        location.href = "../pages/adminPage.html"
-                    }else{
-                        location.href = "../pages/userPage.html"
-                    }
-
-                }
-            })
-            if(!userFound){
-                alert("Login e/ou senha inválidos")
-            }
+                        location.href = "../pages/adminPage.html";
+                    } else{
+                        location.href = "../pages/userPage.html";
+                        usuarioErro.innerHTML = "";
+                        senhaErro.innerHTML = "";
+                    };
+                } else {
+                    if(login != element.usuario && password == element.senha) {
+                        usuarioErro.innerHTML = "Usuário digitado é inválido."
+                        usuarioErro.style.color = "#ff0000";
+                    } else if(login == element.usuario && password != element.senha) {
+                        senhaErro.innerHTML = "Senha digitada é invalida."
+                        senhaErro.style.color = "#ff0000"; 
+                    } else {
+                        usuarioErro.innerHTML = "Usuário digitado é inválido."
+                        usuarioErro.style.color = "#ff0000";
+                        senhaErro.innerHTML = "Senha digitada é invalida."
+                        senhaErro.style.color = "#ff0000";    
+                 }
+                  
+                }                                                                                     
+            })                    
         })
 }
+
+
+
+// if (login == element.usuario && password != element.senha) {
+//     senhaErro.innerHTML = "Senha digitada é invalida."
+//     senhaErro.style.color = "#00ff00"; 
+// }  else if (login != element.usuario && password == element.senha) {
+//     usuarioErro.innerHTML = "Usuário digitado é inválido."
+//     usuarioErro.style.color = "#00ff00";
+// }
+// else if (login != element.usuario && password != element.senha) {
+//         usuarioErro.innerHTML = "Usuário digitado é inválido."
+//         usuarioErro.style.color = "#00ff00";
+//         senhaErro.innerHTML = "Senha digitada é invalida."
+//         senhaErro.style.color = "#00ff00";    
+//  } 
