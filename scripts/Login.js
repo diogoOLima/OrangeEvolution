65cc12ae3fb1f9edd1be109f0f7d50efcc88e8b9
Lucas_Passos <lucas_passos@msn.com>
2022-11-10 18:55:25 -0300

    Adicao de validacao na tela de login - usuario e senha

--- scripts/Login.js
+++ scripts/Login.js
@@ -6,7 +6,7 @@ const senhaErro = document.getElementById('erro-senha');
 function getUsers(){
     axios.get(url)
         .then(response => {
-            const data = response.data
+            const data = response.data;
             const login = document.querySelector('#ilogin').value;
             const password = document.querySelector('#ipassword').value;
             
@@ -17,9 +17,12 @@ function getUsers(){
                 isAdmin: ''
             }
 
+            let erroUSuariosenha = false;
             data.forEach(element => {
                 if(login == element.usuario && password == element.senha){
-
+                    erroUSuariosenha = true;
+                    usuarioErro.innerHTML = "";
+                    senhaErro.innerHTML = "";
                     let token = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2);
                     localStorage.setItem('token', token)
                     userValid = {
@@ -32,25 +35,45 @@ function getUsers(){
                     if(element.isAdmin == 1){
                         location.href = "../pages/adminPage.html";
                     } else{
-                        location.href = "../pages/userPage.html";
-                        usuarioErro.innerHTML = "";
-                        senhaErro.innerHTML = "";
+                        location.href = "../pages/userPage.html";                       
                     };
                 } else {
                     if(login != element.usuario && password == element.senha) {
                         usuarioErro.innerHTML = "Usuário digitado é inválido."
                         usuarioErro.style.color = "#ff0000";
-                    } else if(login == element.usuario && password != element.senha) {
+                        usuarioErro.style.width = "65%"
+                        usuarioErro.style.display = "flex";
+                        usuarioErro.style.justifyContent = "flex-start"
+                        usuarioErro.style.fontSize = "12px";
+                        senhaErro.innerHTML = ""
+                        erroUSuariosenha = true;
+                    } 
+                    if(login == element.usuario && password != element.senha) {
                         senhaErro.innerHTML = "Senha digitada é invalida."
                         senhaErro.style.color = "#ff0000"; 
-                    } else {
+                        senhaErro.style.width = "65%"
+                        senhaErro.style.display = "flex";
+                        senhaErro.style.justifyContent = "flex-start"
+                        senhaErro.style.fontSize = "12px";
+                        usuarioErro.innerHTML = ""
+                        erroUSuariosenha = true;
+                    }
+                    
+                    if(!erroUSuariosenha) {
+                        senhaErro.innerHTML = "Senha digitada é invalida."
+                        senhaErro.style.color = "#ff0000"; 
+                        senhaErro.style.width = "65%"
+                        senhaErro.style.display = "flex";
+                        senhaErro.style.justifyContent = "flex-start"
+                        senhaErro.style.fontSize = "12px";
                         usuarioErro.innerHTML = "Usuário digitado é inválido."
                         usuarioErro.style.color = "#ff0000";
-                        senhaErro.innerHTML = "Senha digitada é invalida."
-                        senhaErro.style.color = "#ff0000";    
-                 }
-                  
-                }                                                                                     
+                        usuarioErro.style.width = "65%"
+                        usuarioErro.style.display = "flex";
+                        usuarioErro.style.justifyContent = "flex-start"
+                        usuarioErro.style.fontSize = "12px";
+                    }
+                }                                                                                            
             })                    
         })
 }
