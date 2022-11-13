1fb006b71ae1d993f43158a999a361075a56957b
Diogo Oliveira de Lima <97170322+diogoOLima@users.noreply.github.com>
2022-11-13 16:15:48 -0300

    Página de admin semi-pronta
    A pagina esta com boa parte da estilização e conteudo finalizado, já fizemos a parte de inserção de remoção de novas aulas. Estaremos agora pensando e fazendo a lógica de edição de uma aula

---
+++ js/conteudoTrilhaFTAdm.js
@@ -0,0 +1,137 @@
+
+
+
+const url = "https://orangevolution.herokuapp.com/aulas"
+const conteudos = document.querySelector('#conteudos')
+const infoProgressoDOM = document.querySelector('#infoProgresso')
+const progresso = document.querySelector("#barra div")
+
+
+var countConcluido = 0;
+let countAulas = 0;
+conteudos.style.color = "white"
+
+function fazPost(url, body){
+    axios.post(url, body)
+        .then(response => {
+            alert(JSON.stringify(response.data))
+            console.log(JSON.stringify(response.data))
+            getAulas()
+        })
+}
+
+// form.addEventListener("submit", function(){
+
+//     console.log(formNome.value)
+//     console.log(formLink.value)
+
+//     body = {
+//         "nomeAula": formNome,
+//         "link": formLink,
+//         "tempo": formDuracao,
+//         "origem": formOrigem,
+//         "tipo": formTipo
+//     }
+// })
+
+function cadastroAula(){
+    event.preventDefault()
+    let formLink = document.querySelector('#link').value
+    let formNome = document.querySelector('#nome').value
+    let formTipo = document.querySelector('#tipo').value
+    let formDuracao = document.querySelector('#duracao').value
+    let formOrigem = document.querySelector('#origem').value
+    
+    body = {
+        "nomeAula": formNome,
+        "link": formLink,
+        "tempo": formDuracao,
+        "origem": formOrigem,
+        "tipo": formTipo
+    }
+
+
+    fazPost(url, body)
+}
+
+function removerAula(id){
+    id = id.slice(6)
+    axios.delete(`${url}/${id}`)
+    .then(response =>{
+        alert(`Aula de id = ${id} foi removido com sucesso!`)
+        getAulas()
+        window.location.reload();
+    })
+}
+
+function getAulas(){
+    axios.get(url)
+        .then(response => {
+            console.log(response.data);
+            const data = response.data
+
+            const nomeTrilha = ["Artigo FullStack", "Video FullStack", "Curso FullStack"]
+
+
+
+            nomeTrilha.forEach(element => {
+                data.forEach(conteudo => {
+                    if (conteudo.tipo == element){
+                        countAulas++;
+                        let aula = {
+                            nome: conteudo.nomeAula,
+                            duração: conteudo.tempo,
+                            origem: conteudo.origem,
+                            link: conteudo.link,
+                            aulaId: conteudo.aula_id
+                        }
+                        conteudos.innerHTML += ` 
+                        <div class="card card-borda">
+                                <div class="card-body">
+                                    <div class="row" style="display: flex;align-items: center;justify-content: center;">
+                                        <div class="col-md-5"><div><small class="text-info">Artigo</small><p>${aula.nome}</p></div></div>
+                                        <div class="col-md-2 col-5 mt-4 mb-2 mt-sm-0"><p><small class="d-md-none text-info">Tempo: </small> ${aula.duração}</p></div>
+                                        <div class="col-md-2 col-7 mt-4 mb-2 mt-sm-0"><p><font class="d-md-none text-info">Origem: </font>${aula.origem}</p></div>
+                                        <div class="col-md-1 col-3"><button onclick="" class="btn-admin"><img src="../images/edit.png"></button></div>
+                                        <div class="col-md-1 col-8"><button onclick="removerAula(id)" class="btn-admin" id="aulaId${aula.aulaId}"><img src="../images/delete.png"></button></div>
+                                        <div class="col-md-1 col-1"><img src="../images/points.png"></div>
+                                        
+                                    </div>
+                                </div>
+                        </div>
+                           
+                            <br>
+                        `
+                    }
+
+                })
+            })
+            // calcularProgresso()
+        })
+}
+
+// function altStatusButton(id){
+//     let status = document.querySelector(`#${id}`)
+
+//     if(status.textContent === "Pendente"){
+//         status.textContent = "Concluido"
+//         status.style.backgroundColor = "green"
+//         countConcluido++
+//         calcularProgresso()
+//     }else{
+//         status.textContent = "Pendente"
+//         status.style.backgroundColor = "#CA1C1C"
+//         countConcluido--
+//         calcularProgresso()
+//     }
+
+// }
+
+// function calcularProgresso() {
+//     let progressoAtual = (countConcluido / countAulas) * 100;
+//     progresso.style.width= `${progressoAtual}%`
+//     infoProgressoDOM.innerHTML = `${~~progressoAtual}% - ${countConcluido}/${countAulas}<br>`
+// }
+
+// cadastroAula()
+getAulas();
